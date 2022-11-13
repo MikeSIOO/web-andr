94af69ac27856fa7db355eb82529500e9d94badb
MikeSIOO <m.pazhitnov@yandex.ru>
2022-11-13 17:16:53 +0300

    Модели в БД и их отображение во вьюхах

--- app/models.py
+++ app/models.py
@@ -1,18 +1,106 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
-QUESTIONS = [
-    {
-        'id': question_id,
-        'title': f'Question #{question_id}',
-        'text': f'Text of question #{question_id}',
-        'tags': ['black-jack', 'bender'],
-        'answers': [
-            {
-                'id': answer_id,
-                'text': f'Text of answer #{answer_id}',
-            } for answer_id in range(2)
-        ]
-    } for question_id in range(30)
-]
+
+class TagManager(models.Manager):
+    def popular(self):
+        return self.order_by('-usage_num')[:10]
+
+
+class ProfileManager(models.Manager):
+    def best(self):
+        return self.order_by('-rating')[:5]
+
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by("-date_create")
+
+    def hot(self):
+        return self.order_by("-rating")
+
+    def by_tag(self, tag_name):
+        return self.filter(tags__name=tag_name)
+
+
+class AnswerManager(models.Manager):
+    def new(self, id):
+        q = Question.objects.get(pk=id)
+        return self.filter(question=q).order_by("-date_create")
+
+    def hot(self, id):
+        q = Question.objects.get(pk=id)
+        return self.filter(question=q).order_by("-rating")
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    usage_num = models.PositiveIntegerField(default=0)
+
+    objects = TagManager()
+
+    def __str__(self):
+        return '{} {}'.format(self.name, self.usage_num)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # email = models.CharField(max_length=30, unique=True)
+    # nick_name = models.CharField(max_length=30, unique=True)
+    # password = models.CharField(max_length=30)
+    # avatar = models.ImageField()
+    # date_create = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+
+    objects = ProfileManager()
+
+    def __str__(self):
+        return self.user.username
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    text = models.TextField()
+    author = models.ForeignKey(Profile, models.SET_NULL, null=True)
+    date_create = models.DateField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    rating = models.IntegerField(default=0)
+
+    objects = QuestionManager()
+
+    def __str__(self):
+        return '{} {}'.format(self.title, self.rating)
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(Profile, models.SET_NULL, null=True)
+    date_create = models.DateField(auto_now_add=True)
+    correct = models.BooleanField(default=False)
+    rating = models.IntegerField(default=0)
+    question = models.ForeignKey(Question, models.CASCADE)
+
+    objects = AnswerManager()
+
+    def __str__(self):
+        return '{} {}'.format(self.rating, self.text)
+
+# 1.45
+
+
+# QUESTIONS = [
+#     {
+#         'id': question_id,
+#         'title': f'Question #{question_id}',
+#         'text': f'Text of question #{question_id}',
+#         'tags': ['black-jack', 'bender'],
+#         'answers': [
+#             {
+#                 'id': answer_id,
+#                 'text': f'Text of answer #{answer_id}',
+#             } for answer_id in range(2)
+#         ]
+#     } for question_id in range(30)
+# ]
--- app/views.py
+++ app/views.py
@@ -3,28 +3,37 @@ import string
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from . import models
+from .models import Profile, Tag, Question, Answer
+
 
 # Create your views here.
 
 
 def index(request):
-    context = {'questions': paginate(list(models.QUESTIONS), request), 'hot': False}
-    return render(request, 'index.html', context=context)
+    new_questions = Question.objects.new()
+    context['questions'] = paginate(new_questions, request)
+    context['hot'] = False
+    return render(request, 'index.html', context)
 
 
 def hot(request):
-    context = {'questions': paginate(list(models.QUESTIONS), request), 'hot': True}
-    return render(request, 'index.html', context=context)
+    hot_questions = Question.objects.hot()
+    context['questions'] = paginate(hot_questions, request)
+    context['hot'] = True
+    return render(request, 'index.html', context)
 
 
 def tag(request, tag_name: string):
-    context = {'questions': paginate(list(models.QUESTIONS), request), 'tag_name': tag_name}
-    return render(request, 'tag_search.html', context=context)
+    questions_by_tag = Question.objects.by_tag(tag_name)
+    context['questions'] = paginate(questions_by_tag, request)
+    context['tag_name'] = tag_name
+    return render(request, 'tag_search.html', context)
 
 
 def question(request, question_id: int):
-    context = {'question': models.QUESTIONS[question_id]}
-    return render(request, 'single_question.html', context=context)
+    context['question'] = Question.objects.get(pk=question_id)
+    context['answers'] = Answer.objects.hot(question_id)
+    return render(request, 'single_question.html', context)
 
 
 def login(request):
@@ -44,7 +53,8 @@ def settings(request):
 
 
 def paginate(object_list, request):
-    paginator = Paginator(object_list, 3)
+    # paginator = Paginator(object_list, 3)
+    paginator = Paginator(list(object_list), 3)
     page = request.GET.get('page')
     try:
         if int(page) > paginator.num_pages or int(page) < 1:
@@ -54,3 +64,9 @@ def paginate(object_list, request):
     except:
         objects_page = paginator.get_page(1)
     return objects_page
+
+
+context = {
+    'popular_tags': Tag.objects.popular(),
+    'best_members': Profile.objects.best(),
+}
